8221757eebc3f1a08497490c9203d7f01ebd1adc
nosilaepilef <nosilaepilef@gmail.com>
2021-02-10 12:05:28 -0400

    Layout improvements

--- src/pages/etiqueta/index.js
+++ src/pages/etiqueta/index.js
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./index.css";
 import Logo from "../../assets/logo_grafitte.svg";
 import { useParams } from "react-router-dom";
-import Api from "../../services/Api"
+import Api from "../../services/Api";
 import mask from "../../utils/mask";
+import QRcode from 'qrcode.react';
+import BounceLoader from "react-spinners/BounceLoader";
+import BarLoader from "react-spinners/BarLoader"
 
 function Etiqueta({ match }) {
   const id = useParams();
@@ -12,11 +15,8 @@ function Etiqueta({ match }) {
   const [etiqueta, setEtiqueta] = useState("");
 
   useEffect(() => {
-    Api.post("/pedido", { pedido }).then(r => setEtiqueta(r.data))
-  }, [pedido])
-
-  console.log(etiqueta.pedido)
-  console.log(pedido)
+    Api.post("/pedido", { pedido }).then((r) => setEtiqueta(r.data));
+  }, [pedido]);
   
   return (
     <div className="container--pai">
@@ -44,7 +44,10 @@ function Etiqueta({ match }) {
 
             <div className="linha--h">
               <div className="titles de">
-                <span className="iconify" data-icon="carbon:delivery-parcel"></span>
+                <span
+                  className="iconify"
+                  data-icon="carbon:delivery-parcel"
+                ></span>
                 <h3>Detalhes da Entregas</h3>
               </div>
             </div>
@@ -53,22 +56,36 @@ function Etiqueta({ match }) {
               <div className="dados--de">
                 <span>Enviar pedido via:</span>
                 <p>
-                  <strong>{
-                    etiqueta.pedido ? 
-                    etiqueta.pedido.enviado === 'ELG' ? 
-                    'Entrega - Lojas Grafitte - ELG' :
-                    etiqueta.pedido.enviado === 'RLGV' ? 
-                    'Retirar - Loja Várzea Grande - RLGV' :
-                    etiqueta.pedido.enviado === 'RLGC' ? 
-                    'Retirar - Loja Cuiabá Coxipó - RLGC' : null: 'Sem Registro!'
-                  }</strong>
+                  <strong>
+                    {etiqueta.pedido
+                      ? etiqueta.pedido.enviado === "ELG"
+                        ? "Entrega - Lojas Grafitte - ELG"
+                        : etiqueta.pedido.enviado === "RLGV"
+                        ? "Retirar - Loja Várzea Grande - RLGV"
+                        : etiqueta.pedido.enviado === "RLGC"
+                        ? "Retirar - Loja Cuiabá Coxipó - RLGC"
+                        : etiqueta.pedido.enviado === "Correios SEDEX"
+                        ? "Transportadora Correios - SEDEX"
+                        : etiqueta.pedido.enviado === "Correios PAC"
+                        ? "Transportadora Correios - PAC"
+                        : etiqueta.pedido.enviado ===
+                          "Jadlog Package - Transportadora "
+                        ? "Transportadora - Jadlog Package"
+                        : null
+                      : <BarLoader color={"#d3d3d3"} loading={true} size={10} />
+                    }
+                  </strong>
                 </p>
               </div>
 
               <div className="dados--de">
                 <span>NP - (Número de pedido):</span>
                 <p>
-                  <strong className="red">#{etiqueta.pedido ? etiqueta.pedido.codigoPedido : 'Sem registro!'}</strong>
+                  <strong className="red">
+                    {etiqueta.pedido
+                      ? `#${etiqueta.pedido.codigoPedido}`
+                      : <BarLoader color={"#ff7272"} loading={true} size={10} />}
+                  </strong>
                 </p>
               </div>
             </div>
@@ -87,22 +104,42 @@ function Etiqueta({ match }) {
             <div className="dados--destinatario">
               <div className="dados--de">
                 <span>Nome:</span>
-                <p><strong>{etiqueta.client ? etiqueta.client.nome : "Sem Registro"}</strong></p>
+                <p>
+                  <strong>
+                    {etiqueta.client ? etiqueta.client.nome : <BarLoader color={"#d3d3d3"} loading={true} size={10} />}
+                  </strong>
+                </p>
               </div>
 
               <div className="dados--de">
                 <span>CPF:</span>
-                <p><strong>{etiqueta.client ? mask.cpfMask(etiqueta.client.cpf) : "Sem Registro"}</strong></p>
+                <p>
+                  <strong>
+                    {etiqueta.client
+                      ? mask.cpfMask(etiqueta.client.cpf)
+                      : <BarLoader color={"#d3d3d3"} loading={true} size={10} />}
+                  </strong>
+                </p>
               </div>
 
               <div className="dados--de">
                 <span>E-mail:</span>
-                <p><strong>{etiqueta.client ? etiqueta.client.email : "Sem Registro"}</strong></p>
+                <p>
+                  <strong>
+                    {etiqueta.client ? etiqueta.client.email : <BarLoader color={"#d3d3d3"} loading={true} size={10} />}
+                  </strong>
+                </p>
               </div>
 
               <div className="dados--de">
                 <span>Celular:</span>
-                <p><strong>{etiqueta.client ? mask.celularMask(etiqueta.client.celular) : "Sem Registro"}</strong></p>
+                <p>
+                  <strong>
+                    {etiqueta.client
+                      ? mask.celularMask(etiqueta.client.celular)
+                      : <BarLoader color={"#d3d3d3"} loading={true} size={10} />}
+                  </strong>
+                </p>
               </div>
             </div>
 
@@ -120,32 +157,114 @@ function Etiqueta({ match }) {
             <div className="dados--destinatario">
               <div className="dados--de">
                 <span>Endereço:</span>
-                <p><strong>{etiqueta.entrega ? etiqueta.entrega.endereco : "Sem Registro"}</strong></p>
+                <p>
+                  <strong>
+                    {etiqueta.entrega
+                      ? etiqueta.entrega.endereco
+                      : <BarLoader color={"#d3d3d3"} loading={true} size={10} />}
+                  </strong>
+                </p>
               </div>
 
               <div className="dados--de">
                 <span>Número:</span>
-                <p><strong>{etiqueta.entrega ? etiqueta.entrega.numero < 10 ? `0${etiqueta.entrega.numero}` : etiqueta.entrega.numero: "Sem Registro"}</strong></p>
+                <p>
+                  <strong>
+                    {etiqueta.entrega
+                      ? etiqueta.entrega.numero < 10
+                        ? `0${etiqueta.entrega.numero}`
+                        : etiqueta.entrega.numero
+                      : <BarLoader color={"#d3d3d3"} loading={true} size={10} />}
+                  </strong>
+                </p>
                 <span>Bairro:</span>
-                <p><strong>{etiqueta.entrega ? etiqueta.entrega.bairro : "Sem Registro"}</strong></p>
+                <p>
+                  <strong>
+                    {etiqueta.entrega
+                      ? etiqueta.entrega.bairro
+                      : <BarLoader color={"#d3d3d3"} loading={true} size={10} />}
+                  </strong>
+                </p>
               </div>
 
               <div className="dados--de">
                 <span>Cidade:</span>
-                <p><strong>{etiqueta.entrega ? etiqueta.entrega.cidade : "Sem Registro"}</strong></p>
+                <p>
+                  <strong>
+                    {etiqueta.entrega
+                      ? etiqueta.entrega.cidade
+                      : <BarLoader color={"#d3d3d3"} loading={true} size={10} />}
+                  </strong>
+                </p>
                 <span>Estado:</span>
-                <p><strong>{etiqueta.entrega ? etiqueta.entrega.estado : "Sem Registro"}</strong></p>
+                <p>
+                  <strong>
+                    {etiqueta.entrega
+                      ? etiqueta.entrega.estado
+                      : <BarLoader color={"#d3d3d3"} loading={true} size={10} />}
+                  </strong>
+                </p>
               </div>
 
               <div className="dados--de">
                 <span>CEP:</span>
-                <p><strong>{etiqueta.entrega ? mask.cepMask(etiqueta.entrega.cep) : "Sem Registro"}</strong></p>
+                <p>
+                  <strong>
+                    {etiqueta.entrega
+                      ? mask.cepMask(etiqueta.entrega.cep)
+                      : <BarLoader color={"#d3d3d3"} loading={true} size={10} />}
+                  </strong>
+                </p>
               </div>
 
-              <div className="dados--de">
-                
-                <p className="cto">Complemento: <strong>{etiqueta.entrega ? etiqueta.entrega.complemento : "Sem Registro"}</strong></p>
+              {etiqueta.entrega 
+                ? etiqueta.entrega.complemento 
+                ? (
+                <div className="dados--de">
+                  <p className="cto">
+                    Complemento:
+                    <strong>
+                      {etiqueta.entrega.complemento ? etiqueta.entrega.complemento : <BarLoader color={"#d3d3d3"} loading={true} size={10} />}
+                    </strong>
+                  </p>
+                </div>
+              ) : null : null}
+            </div>
+
+            <div className="linha--h">
+              <div className="titles">
+              <span
+              className="iconify"
+              data-icon="uil:qrcode-scan"
+              data-inline="false">
+              </span>
+                <h3>QRCode Localização:</h3>
               </div>
+            </div>
+
+            <div className="dados--destinatario">
+              <div className="container--qrcode">                                
+                  <p className="qr--text">
+                    Escaneie o QR Code para obter a localização do destinatário
+                    em tempo real. Abra o aplicativo da câmera aponte para o QR Code por 2 a 3 segundos depois de escaneado será exibida uma notificação. Pronto, agora é só escolher aonde
+                    quer abrir o link da localização.
+                  </p>
+                  <span className="load">
+                  {etiqueta ?
+                    <QRcode
+                      value={`https://www.google.com/maps/search/${etiqueta.entrega.endereco.replace(/[- ]/g, "+")},${etiqueta.entrega.numero},${etiqueta.entrega.cidade.replace(/[ ]/g, "+")},${etiqueta.entrega.estado}`}
+                      size={100}
+                      bgColor={"#ffffff"}
+                      fgColor={"#1877F2"}
+                      level={"L"}
+                      includeMargin={false}
+                      renderAs={"svg"}
+                    /> : <BounceLoader color={"#409fff"} loading={true} size={60} />
+                    
+                    }
+                  </span>                 
+              </div>
+              
             </div>
           </div>
         </div>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,18 +1,66 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./index.css";
 import Logo from "../../assets/logo_grafitte.svg";
 import { IoMdSearch } from "react-icons/io";
 import Api from "../../services/Api";
-import mask from '../../utils/mask'
+import mask from "../../utils/mask";
+import ContentLoader from "react-content-loader";
 
 function Home() {
-  const [pedido, setPedido] = useState("")
-  const [dados, setDados] = useState('')
-  function handlerSubmit(e){
-    e.preventDefault()
-    Api.post("/pedido", {pedido}).then(r => setDados(r.data))
+  const [pedido, setPedido] = useState("");
+  const [dados, setDados] = useState("");
+  const [load, setLoad] = useState(false);
+  const [header, setHeader] = useState(false);
+
+  function handlerSubmit(e) {
+    setLoad(true);
+    setHeader(true);
+    e.preventDefault();
+    Api.post("/pedido", { pedido }).then((r) => {
+      setDados(r.data);
+      setLoad(false);
+    });
   }
+
+  const loaderTwoRow = () => (
+    <ContentLoader viewBox="-30 10 450 70">
+      <rect x="0" y="17" rx="4" ry="4" width="70" height="13" />
+      <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+
+      <rect x="0" y="35" rx="4" ry="4" width="70" height="13" />
+      <rect x="80" y="35" rx="4" ry="4" width="300" height="13" />
+    </ContentLoader>
+  );
+
+  const loaderThreeRow = () => (
+    <ContentLoader viewBox="-30 10 450 70">
+      <rect x="0" y="17" rx="4" ry="4" width="70" height="13" />
+      <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+
+      <rect x="0" y="35" rx="4" ry="4" width="70" height="13" />
+      <rect x="80" y="35" rx="4" ry="4" width="300" height="13" />
+
+      <rect x="0" y="53" rx="4" ry="4" width="70" height="13" />
+      <rect x="80" y="53" rx="4" ry="4" width="300" height="13" />
+    </ContentLoader>
+  );
+
+  const loaderForRow = () => (
+    <ContentLoader viewBox="-30 10 450 85">
+      <rect x="0" y="17" rx="4" ry="4" width="70" height="13" />
+      <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+
+      <rect x="0" y="35" rx="4" ry="4" width="70" height="13" />
+      <rect x="80" y="35" rx="4" ry="4" width="300" height="13" />
+
+      <rect x="0" y="53" rx="4" ry="4" width="70" height="13" />
+      <rect x="80" y="53" rx="4" ry="4" width="300" height="13" />
+
+      <rect x="0" y="71" rx="4" ry="4" width="70" height="13" />
+      <rect x="80" y="71" rx="4" ry="4" width="300" height="13" />
+    </ContentLoader>
+  );
 
   return (
     <>
@@ -24,12 +72,13 @@ function Home() {
             </Link>
           </div>
           <div className="pesquisa--id">
-            <form onSubmit={e=> handlerSubmit(e)}>
-              <input 
+            <form onSubmit={(e) => handlerSubmit(e)}>
+              <input
                 placeholder="Pesquisar pelo ID do pedido"
+                autoFocus={true}
                 type="text"
                 value={pedido}
-                onChange={e => setPedido(e.target.value)}
+                onChange={(e) => setPedido(e.target.value)}
               />
 
               <button title="Pesquisar">
@@ -39,131 +88,218 @@ function Home() {
           </div>
         </div>
       </nav>
-      <section>
-        <div className="container--cb">
-          <div className="title">
-            <h2>Tipo de Entrega</h2>
-          </div>
 
-          <div className="btn whats">
-            <a href={`https://wa.me/55${dados.client ? dados.client.celular : null}`} target="_blank" rel="noreferrer">
-              <button>
-                <span className="iconify" data-icon="fa-brands:whatsapp" data-inline="false"></span>
-              </button>
-              </a>
-          </div>
+      {header ? (
+        <>
+          <section>
+            <div className="container--cb">
+              <div className="title">
+                <h2>Tipo de Entrega</h2>
+              </div>
 
-          <div className="btn print">
-            <Link to={`/etiqueta/${pedido}`}>
-              <button>
+              <div className="btn whats">
+                <a
+                  href={`https://wa.me/55${
+                    dados.client ? dados.client.celular : null
+                  }`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button>
+                    <span
+                      className="iconify"
+                      data-icon="fa-brands:whatsapp"
+                      data-inline="false"
+                    ></span>
+                  </button>
+                </a>
+              </div>
+
+              <div className="btn print">
+                <Link to={`/etiqueta/${pedido}`}>
+                  <button>
+                    <span
+                      className="iconify"
+                      data-icon="ion:print-outline"
+                      data-inline="false"
+                    ></span>
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="tipo--entrega">
+              <div className="dados">
                 <span
                   className="iconify"
-                  data-icon="ion:print-outline"
+                  data-icon="carbon:delivery-parcel"
+                ></span>
+                <h3>Detalhes da Entregas</h3>
+              </div>
+
+              {load ? (
+                loaderTwoRow()
+              ) : (
+                <>
+                  <div className="info--de">
+                    <span>Enviar pedido via:</span>
+                    <p>
+                      {dados.pedido
+                        ? dados.pedido.enviado === "ELG"
+                          ? "Entrega - Lojas Grafitte - ELG"
+                          : dados.pedido.enviado === "RLGV"
+                          ? "Retirar - Loja Várzea Grande - RLGV"
+                          : dados.pedido.enviado === "RLGC"
+                          ? "Retirar - Loja Cuiabá Coxipó - RLGC"
+                          : dados.pedido.enviado === "Correios SEDEX"
+                          ? "Transportadora Correios - SEDEX"
+                          : dados.pedido.enviado === "Correios PAC"
+                          ? "Transportadora Correios - PAC"
+                          : dados.pedido.enviado ===
+                            "Jadlog Package - Transportadora "
+                          ? "Transportadora - Jadlog Package"
+                          : null
+                        : "Sem Registro!"}
+                    </p>
+                  </div>
+
+                  <div className="info--de">
+                    <span>Número do pedido:</span>
+                    <p>
+                      <strong>
+                        #
+                        {dados.pedido
+                          ? dados.pedido.codigoPedido
+                          : "Sem registro"}
+                      </strong>
+                    </p>
+                  </div>
+                </>
+              )}
+            </div>
+          </section>
+
+          <section>
+            <div className="title">
+              <h2>Dados da Etiqueta</h2>
+            </div>
+          </section>
+
+          <section>
+            <div className="dados--etiqueta">
+              <div className="dados">
+                <span
+                  className="iconify"
+                  data-icon="bx:bx-user-circle"
                   data-inline="false"
                 ></span>
-              </button>
-            </Link>
-          </div>
-        </div>
-      </section>
+                <h3>Dados do Cliente</h3>
+              </div>
 
-      <section>
-        <div className="tipo--entrega">
-          <div className="dados">
-            <span className="iconify" data-icon="carbon:delivery-parcel"></span>
-            <h3>Detalhes da Entregas</h3>
-          </div>
+              {load ? (
+                loaderForRow()
+              ) : (
+                <>
+                  <div className="info--de">
+                    <span>Nome:</span>
+                    <p>{dados.pedido ? dados.client.nome : "Sem registro"}</p>
+                  </div>
 
-          <div className="info--de">
-            <span>Enviar pedido via:</span>
-            <p>{
-              dados.pedido ? 
-              dados.pedido.enviado === 'ELG' ? 
-              'Entrega - Lojas Grafitte - ELG' :
-              dados.pedido.enviado === 'RLGV' ? 
-              'Retirar - Loja Várzea Grande - RLGV' :
-              dados.pedido.enviado === 'RLGC' ? 
-              'Retirar - Loja Cuiabá Coxipó - RLGC' : null: 'Sem Registro!'
-            }</p>
-          </div>
+                  <div className="info--de">
+                    <span>CPF:</span>
+                    <p>
+                      {dados.pedido
+                        ? mask.cpfMask(dados.client.cpf)
+                        : "Sem registro"}
+                    </p>
+                  </div>
 
-          <div className="info--de">
-            <span>Número do pedido:</span>
+                  <div className="info--de">
+                    <span>E-mail:</span>
+                    <p>{dados.pedido ? dados.client.email : "Sem registro"}</p>
+                  </div>
+
+                  <div className="info--de">
+                    <span>Celular:</span>
+                    <p>
+                      {dados.pedido
+                        ? mask.celularMask(dados.client.celular)
+                        : "Sem registro"}
+                    </p>
+                  </div>
+                </>
+              )}
+
+              <div className="dados">
+                <span
+                  className="iconify"
+                  data-icon="feather:map-pin"
+                  data-inline="false"
+                ></span>
+                <h3>Endereço de Entrega</h3>
+              </div>
+
+              {load ? (
+                loaderThreeRow()
+              ) : (
+                <>
+                  <div className="info--de">
+                    <span>CEP:</span>
+                    <p>
+                      {dados.pedido
+                        ? mask.cepMask(dados.entrega.cep)
+                        : "Sem registro"}
+                    </p>
+                    <span>Endereço:</span>
+                    <p>
+                      {dados.pedido ? dados.entrega.endereco : "Sem registro"}
+                    </p>
+                  </div>
+
+                  <div className="info--de">
+                    <span>Número:</span>
+                    <p>
+                      {dados.entrega
+                        ? dados.entrega.numero < 10
+                          ? `0${dados.entrega.numero}`
+                          : dados.entrega.numero
+                        : "Sem Registro"}
+                    </p>
+                    <span>Bairro:</span>
+                    <p>
+                      {dados.pedido ? dados.entrega.bairro : "Sem registro"}
+                    </p>
+                  </div>
+
+                  <div className="info--de">
+                    <span>Cidade:</span>
+                    <p>
+                      {dados.pedido ? dados.entrega.cidade : "Sem registro"}
+                    </p>
+                    <span>Estado:</span>
+                    <p>
+                      {dados.pedido ? dados.entrega.estado : "Sem registro"}
+                    </p>
+                  </div>
+                </>
+              )}
+            </div>
+          </section>
+        </>
+      ) : (
+        <div className="container--bemvindo">
+          <div className="bemvindo">
+            <h1>🤩 Seja bem vindo, Mestre! 🤝</h1>
+            <p>Etiqueta de qual cliente deseja gerar mestre?</p>
             <p>
-              <strong>#{dados.pedido ? dados.pedido.codigoPedido : 'Sem registro'}</strong>
+              Por gentileza insira o ID do pedido na barra de pesquisa para que
+              possamos buscar as informações do cliente e gerar a etiqueta.
             </p>
+            <p>Desde já agradeço pela visita, volte sempre! 😍</p>
           </div>
         </div>
-      </section>
-
-      <section>
-        <div className="title">
-          <h2>Dados da Etiqueta</h2>
-        </div>
-      </section>
-
-      <section>
-        <div className="dados--etiqueta">
-          <div className="dados">
-            <span
-              className="iconify"
-              data-icon="bx:bx-user-circle"
-              data-inline="false"
-            ></span>
-            <h3>Dados do Cliente</h3>
-          </div>
-
-          <div className="info--de">
-            <span>Nome:</span>
-            <p>{dados.pedido ? dados.client.nome : 'Sem registro'}</p>
-          </div>
-
-          <div className="info--de">
-            <span>CPF:</span>
-            <p>{dados.pedido ? mask.cpfMask(dados.client.cpf) : 'Sem registro'}</p>
-          </div>
-
-          <div className="info--de">
-            <span>E-mail:</span>
-            <p>{dados.pedido ? dados.client.email : 'Sem registro'}</p>
-          </div>
-
-          <div className="info--de">
-            <span>Celular:</span>
-            <p>{dados.pedido ? mask.celularMask(dados.client.celular) : 'Sem registro'}</p>
-          </div>
-
-          <div className="dados">
-            <span
-              className="iconify"
-              data-icon="feather:map-pin"
-              data-inline="false"
-            ></span>
-            <h3>Endereço de Entrega</h3>
-          </div>
-
-          <div className="info--de">
-            <span>CEP:</span>
-            <p>{dados.pedido ? mask.cepMask(dados.entrega.cep) : 'Sem registro'}</p>
-            <span>Endereço:</span>
-            <p>{dados.pedido ? dados.entrega.endereco : 'Sem registro'}</p>
-          </div>
-
-          <div className="info--de">
-            <span>Número:</span>
-            <p>{dados.entrega ? dados.entrega.numero < 10 ? `0${dados.entrega.numero}` : dados.entrega.numero: "Sem Registro"}</p>
-            <span>Bairro:</span>
-            <p>{dados.pedido ? dados.entrega.bairro : 'Sem registro'}</p>
-          </div>
-
-          <div className="info--de">
-            <span>Cidade:</span>
-            <p>{dados.pedido ? dados.entrega.cidade : 'Sem registro'}</p>
-            <span>Estado:</span>
-            <p>{dados.pedido ? dados.entrega.estado : 'Sem registro'}</p>
-          </div>
-        </div>
-      </section>
+      )}
     </>
   );
 }
